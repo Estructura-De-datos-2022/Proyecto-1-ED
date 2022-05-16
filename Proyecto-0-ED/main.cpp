4e701c5e47c3f63ebf7b55107042aadc1095b66e
kenfabguzram <95146991+kenfabguzram@users.noreply.github.com>
2022-05-15 18:26:19 -0600

    ADD File existing check

--- Proyecto-0-ED/main.cpp
+++ Proyecto-0-ED/main.cpp
@@ -5,12 +5,27 @@
 #include "ArrayList.h"
 #include "Trie.h"
 #include <windows.h>
+#include<stdio.h>
+#include<sys/stat.h>
 using namespace std;
 
 /*
 https://parzibyte.me/blog
+https://www.delftstack.com/es/howto/c/c-check-if-file-exists/#:~:text=completa%20del%20archivo.-,stat()%20Funci%C3%B3n%20para%20comprobar%20si%20un%20archivo%20existe%20en,si%20el%20archivo%20no%20existe.&text=El%20programa%20imprimir%C3%A1%20file%20exists%20si%20el%20archivo%20demo.
 */
+
+int checkIfFileExists(const char* filename){
+    struct stat buffer;
+    int exist = stat(filename,&buffer);
+    if(exist == 0)
+        return 1;
+    else
+        return 0;
+}
 void generarListaLineas(string nombreArchivo,List<KVPair<int,string>>* lista){
+    if(!checkIfFileExists(nombreArchivo.c_str())){
+        throw runtime_error("Error: File not found");
+    }
     ifstream archivoListaLineas(nombreArchivo.c_str());
     string linea;
     int contador=1;
@@ -23,17 +38,21 @@ int main() {
     setlocale(LC_ALL,"spanish");
     SetConsoleCP(1252);
     SetConsoleOutputCP(1252);
+
+    List<KVPair<int,string>>* listaLineas= new ArrayList<KVPair<int,string>>();
+    string archivoPrincipal;
+
     cout<<"Hola!!"<<endl;
     cout<<"A continuación leeremos el archivo que nos proporciones y de acuerdo al número que insertes, realizaremos diversos experimentos en este."<<endl;
     cout<<"Inserte el nombre de archivo con su respectiva ruta y extensión: ";
-    string nombreArchivo;
-    cin>>nombreArchivo;
+
+
+    cin>>archivoPrincipal;
+    generarListaLineas(archivoPrincipal,listaLineas);
+
 
     //Trie ignorar;
     //string archivoIgnorar = "ignorar.txt";
-    List<KVPair<int,string>>* res= new ArrayList<KVPair<int,string>>();
-    generarListaLineas(nombreArchivo,res);
-    res->print();
     //nombreArchivo+=".conf";
     //cout << nombreArchivo << endl;
     /*if(nombreArchivo.su)*/
